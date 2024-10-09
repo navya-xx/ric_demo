c75a7fcd875c082e4454f71795c7e33485b6883c
agrawal <navneet.agrawal@hhi-extern.fraunhofer.de>
2024-10-09 14:30:01 +0200

    asdf

--- scioi_robot_manager/extensions/gui/nodejs_gui/frontend/src/lib/components/custom/plot.svelte
+++ scioi_robot_manager/extensions/gui/nodejs_gui/frontend/src/lib/components/custom/plot.svelte
@@ -1,17 +1,17 @@
+<script>
+  import { stream, botList } from "$lib/stores/stream";
+  import { currentBot } from "$lib/stores/main";
+  import UPlot from "$lib/components/custom/uplot.svelte";
 
-  <script>
-  import { stream} from '$lib/stores/stream';
-  import { currentBot  } from '$lib/stores/main';
-  import UPlot from '$lib/components/custom/uplot.svelte';
-
-
-  const keys = ["estimation.state.v","estimation.state.psi_dot","estimation.state.psi","estimation.state.theta_dot","estimation.state.theta"];
-  $: dataGetter =  stream.requestGetter([$currentBot],keys);
-
-
+  const keys = [
+    "estimation.state.v",
+    "estimation.state.psi_dot",
+    "estimation.state.psi",
+    "estimation.state.theta_dot",
+    "estimation.state.theta",
+    "consensus.dist_from_ref",
+  ];
+  $: dataGetter = stream.requestGetter([$currentBot], keys);
 </script>
 
-
-<UPlot dataGetter={dataGetter}/>
-
-
+<UPlot {dataGetter} />
